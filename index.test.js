ef0cc709e6528e2b5312c8eb40b840b7819f5e93
Margatroid <i@margatroid.xyz>
2020-08-12 23:23:44 +0800

    bump version

--- index.test.js
+++ index.test.js
@@ -9,7 +9,7 @@ const data = fs.readFileSync('./README.md')
 var processor = unified()
   .use(markdown)
   .use(remark2rehype)
-  .use(code)
+  .use(code, {theme: 'dark_plus'})
   .use(html)
 
 // console.log(processor.process(data))
